021aad077b25f3f2516e5b79cb1a7e5d950fa8f5
prakashabhinav7 <7prakashabhinav@gmail.com>
2021-04-25 13:03:09 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This project contains a list of Arduino projects, going from beginner to advanced and will keep getting more projects
+added over time.
+
+List of Projects:
+1. 01_hello_world_blink:
+2. 02_sequential_led_blinks:
+3.
---
+++ UNO R3/02_sequential_led_blinks.ino
@@ -0,0 +1,25 @@
+int LED1 = 13;
+int LED2 = 12;
+int LED3 = 11;
+
+void setup() {
+   pinMode(LED1, OUTPUT);
+   pinMode(LED2, OUTPUT);
+   pinMode(LED3, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(LED1, HIGH);    // turn on LED
+  delay(200);                  // wait
+  digitalWrite(LED2, HIGH);
+  delay(200);
+  digitalWrite(LED3, HIGH);
+  delay(200);
+  digitalWrite(LED1, LOW);     // turn off LED
+  delay(300);
+  digitalWrite(LED2, LOW);
+  delay(300);
+  digitalWrite(LED3, LOW);
+  delay(300);                  // wait before restarting the next iteration of the loop
+}
